0e7b682bffe197aa3a21745860bfe940072d83f7
Stephanie Hill <stephni.hill@gmail.com>
2023-04-03 12:52:42 -0700

    attempt

--- js/app.js
+++ js/app.js
@@ -1,37 +1,58 @@
-const draggableElements = document.getElementById('draggable-content');
+const items = document.querySelectorAll('.item');
+const targets = document.querySelectorAll('.target');
 
-function handleClick(event){
-  let selectElement = event.target;
-  console.log(selectElement);
-  selectElement.addEventListener('dragstart', onDragStart);
+let draggedItem = null;
+
+// Add event listeners to items
+items.forEach(item => {
+  item.addEventListener('dragstart', handleDragStart);
+  item.addEventListener('dragend', handleDragEnd);
+});
+
+// Add event listeners to targets
+targets.forEach(target => {
+  target.addEventListener('dragenter', handleDragEnter);
+  target.addEventListener('dragover', handleDragOver);
+  target.addEventListener('dragleave', handleDragLeave);
+  target.addEventListener('drop', handleDrop);
+});
+
+// Handle drag start event
+function handleDragStart(event) {
+  draggedItem = event.target;
+  event.dataTransfer.setData('text', event.target.textContent);
+  event.target.setAttribute('aria-grabbed', 'true');
 }
 
-function onDragOver(event) {
+// Handle drag end event
+function handleDragEnd(event) {
+  event.target.setAttribute('aria-grabbed', 'false');
+  draggedItem = null;
+}
+
+// Handle drag enter event
+function handleDragEnter(event) {
+  event.preventDefault();
+  event.target.classList.add('highlight');
+}
+
+// Handle drag over event
+function handleDragOver(event) {
   event.preventDefault();
 }
 
-function onDragStart(event){  
-  event
-    .dataTransfer
-    .setData('text/plain', event.target.id);
-  
-  event
-    .currentTarget
-    .style
-    .backgroundColor = 'cyan';
+// Handle drag leave event
+function handleDragLeave(event) {
+  event.target.classList.remove('highlight');
 }
 
-function onDrop(event){
-  const id = event
-    .dataTransfer
-    .getData('text/plain');
-  
-  const draggableElement = document.getElementById(id);
-  console.log(id);
-  const dropzone = event.target;
-  
-  dropzone.appendChild(draggableElement);
-  
+// Handle drop event
+function handleDrop(event) {
+  event.preventDefault();
+  event.target.classList.remove('highlight');
+  const dropText = event.dataTransfer.getData('text');
+  if (dropText === event.target.getAttribute('aria-label').substring(5)) {
+    event.target.classList.add('matched');
+    draggedItem.parentNode.removeChild(draggedItem);
+  }
 }
-
-draggableElements.addEventListener('click', handleClick);
